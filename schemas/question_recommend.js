1f66c9950ef438c7a290ac9fc0e0b13417c28596
githubKudoi <whsahd12@gmail.com>
2022-09-20 17:32:49 +0900

    feat: tag schema skeleton

--- schemas/question_recommend.js
+++ schemas/question_recommend.js
@@ -1,5 +1,21 @@
 const {gql} = require('apollo-server');
 
 module.exports = gql`
+    type Query {
+        tag(testId: ID!): [Tag]
+        myTag(userId: ID!): [Tag]
+    }
 
+    type Mutation {
+        createTag(name: String!, testId: ID!): NormalResponse
+        deleteTag(name: String!, testId: ID!): NormalResponse
+    }
+
+    type Tag {
+        id: ID!
+        name: String!
+        ownerId: String!
+        creationDate: String!
+        testId: ID!
+    }
 `;
