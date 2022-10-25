9cb8216215cefa2aab075bd5ee4267325c326d3b
박세훈 <psh3253@naver.com>
2022-10-25 12:43:01 +0900

    Fix: Graphql Error

--- schemas/index.js
+++ schemas/index.js
@@ -13,6 +13,8 @@ const QuestionVerificationSchema = require('./question_verification');
 const QuestionVerificationResolver = require('../resolvers/question_verification');
 const ReviewNoteSchema = require('./review_note');
 const ReviewNoteResolver = require('../resolvers/review_note');
+const CouponSchema = require('./coupon');
+const CouponResolver = require('../resolvers/coupon');
 const {gql} = require('apollo-server');
 
 const Query = gql`
@@ -39,8 +41,8 @@ const Query = gql`
 `;
 
 const schema = makeExecutableSchema({
-    typeDefs: [Query, AuthSchema, ProfileSchema, QuestionSchema, QuestionRecommendSchema, QuestionSolvingSchema, QuestionVerificationSchema, ReviewNoteSchema],
-    resolvers: [AuthResolver, ProfileResolver, QuestionResolver, QuestionRecommendResolver, QuestionSolvingResolver, QuestionVerificationResolver, ReviewNoteResolver]
+    typeDefs: [Query, AuthSchema, ProfileSchema, QuestionSchema, QuestionRecommendSchema, QuestionSolvingSchema, QuestionVerificationSchema, ReviewNoteSchema, CouponSchema],
+    resolvers: [AuthResolver, ProfileResolver, QuestionResolver, QuestionRecommendResolver, QuestionSolvingResolver, QuestionVerificationResolver, ReviewNoteResolver, CouponResolver]
 });
 
 module.exports = schema;
